8a9c43ce8d13b015ebf1f588041d860db8be74a7
Rafael Aldana <rafaelaldana6@Gmail.com>
2023-02-23 19:51:19 -0800

    buncha stuff

--- src/models/tools/model.js
+++ src/models/tools/model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const toolModel = (sequelize, DataTypes) => sequelize.define('Tools', {
-  brand: { type: DataTypes.STRING, required: true },
+  brand: { type: DataTypes.ENUM('CRAFTSMAN', 'DEWALT', 'Bosch', 'Makita', 'Milwaukee'), required: true },
   color: { type: DataTypes.STRING, required: false },
   cost: { type: DataTypes.INTEGER, required: true },
 });
